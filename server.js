781da3b6c1cab8024166455becb69394b69beae4
Gabrielle Ann Franco <Gabrielle.Franco@edu.sait.ca>
2024-03-22 23:44:17 -0600

    adding the try catch block for the 404 page

--- server.js
+++ server.js
@@ -1,46 +1,45 @@
 "use strict";
-import http from "http";
+import http from 'http';
 import fs from "fs";
 import { data } from "./data.js";
 
 const host = "localhost";
 const port = 4000;
 
-// DONE: create a server listener
-
+//DONE: adding a serverListener
 const serverListener = (req, res) => {
   res.writeHead(200);
-  res.end("This server code is much better!");
-};
+  res.end("This server code runs better");
+}
 
-// DONE: setting up an html listener for the server
+//DONE: adding an html listener for the server code.
 
 const htmlListener = (req, res) => {
-  res.setHeader("Content-Type", "text/html");
+  res.setHeader("Content-Type", "text.html");
   res.writeHead(200);
   res.end(data);
 };
 
-//DONE: split the logic into different functions for the server
+//DONE: adding the createServer block and creating support for the 404 page
 
 const server = http.createServer((req, res) => {
   let filePath = "." + req.url;
-
-  // DONE: create support for a 404 page using the else statement.
-
-  if (filePath === "./about.html"){
-    filePath ="./about.html";
-  } else {
+  if(req.url === "/") {
     filePath = "./index.html";
+  } else if (req.url === "/about") {
+    filePath = "./about.html";
+  } else {
+    filePath = "./404error/404error.html";
   }
 
+
   fs.readFile(filePath, (error, content) => {
-    //DONE: handle errors
-    res.writeHead(200, {"Content-Type": "text/html"});
+    //DONE: set up Handle error
+    res.writeHead(200, { "Content-Type": "text/html"});
     res.end(content);
-  }); 
+  });
 });
 
 server.listen(port, host, () => {
-  console.log(`server is running on http://${host}:${port}`);
-})
+  console.log(`server is running here http://${host}:${port}`);
+});
